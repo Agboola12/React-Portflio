f7f976b1e06a5e544e6bac95c99bff4230838549
Agbooa12 <agboolaelijah077@gmail.com>
2023-08-07 15:14:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,182 @@
+import React from 'react'
+// import { Link } from 'react-router-dom';
+import './App.css';
+import { Link } from 'react-router-dom';
+import agbo from './agbo.jpeg'
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg  ">
+        
+        {/* <button className="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon bg-light"></span>
+        </button> */}
+        {/* <div className="collapse navbar-collapse" id="navbarNav"> */}
+          <ul className="navbar-nav mx-auto" >
+            <li className="nav-item">
+              
+              <Link to="https://www.linkedin.com/in/elijah-iyanuoluwa-agboola-6b2631203" className="nav-link text-light  fs-7"><i className="fa fa-linkedin text-success mr-1" aria-hidden="true"></i>LinkedIn</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="https://github.com/Agboola12" className="nav-link text-light fs-7"><i className="fa fa-github text-success mr-1" aria-hidden="true"></i>Github</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="https://wa.link/f4r799" className="nav-link text-light  fs-7"><i className="fa fa-whatsapp text-success mr-1" aria-hidden="true"></i>Whatsapp</Link>
+            </li>
+          </ul>
+        {/* </div> */}
+      </nav>
+      
+      <header className=''>
+          <div className='container text-center text-light pt-5'>
+                <img src={agbo}  alt="agboimage"/>
+                <h2>I'm Agboola Elijah </h2>
+                <p>A Full Stack Developer who specializes in building fast, beautiful and responsive websites and web applications.</p>
+                <p>Got Anything? Contact me</p>
+                <a href='#project' >  <button class="btn btn-outline-success mt-4 mb-2" > My Projects </button></a> 
+          </div>
+          <div className='container-fluid text-light ' id='project'>
+              <h2 className='text-center'>Projects</h2>
+              <hr />
+              <div className='container' >
+                <div className='row '>
+                    <div className='col-md-4 '>
+                    <div className='card    text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>Jahnus Bank Management</h5>
+                          <small className='card-text'>Bank's User Website and the Admin ends</small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Angular, Bootstrap</small><br/>
+                          <Link to="https://elijaybank.netlify.app"  className='text-light'><i class="fa fa-globe" aria-hidden="true"></i>visit</Link>
+                        </div>
+                      </div>
+                    </div>
+
+                      <div className='col-md-4 '>
+                    <div className='card    text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>Love Calculator App</h5>
+                          <small className='card-text'>Love Calculator that enables the user to check the percentage of their love with their crush </small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Vuejs, Bootstrap</small><br/>
+                          <Link to="https://elijayloveapp.netlify.app"  className='text-light'><i class="fa fa-globe" aria-hidden="true"></i>visit</Link>
+                        </div>
+                      </div>
+                    </div>
+
+                      <div className='col-md-4 '>
+                    <div className='card   text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>TicTakToc</h5>
+                          <small className='card-text'>Users can play game to entertain themselves</small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Vuejs, Bootstrap</small><br/>
+                          <Link to="https://elijaytictaktoe.netlify.app"  className='text-light'><i class="fa fa-globe" aria-hidden="true"></i>visit</Link>
+                          
+                          </div>
+                      </div>
+                    </div>  
+                </div>
+
+                <div className='row mt-4'>
+                    <div className='col-md-4 '>
+                    <div className='card  text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>Inventory Management System</h5>
+                          <small className='card-text'>Inventory System for a Pharamacy, it makes the work to be easier for the attendance
+                          and then customer can order whatever they need online too</small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Html, Css, Bootstrap and Php </small><br/>
+                          <Link to="https://github.com/Agboola12/Inventory-System"  className='text-light'><i class="fa fa-globe" aria-hidden="true"></i>visit</Link>
+                        </div>
+                      </div>
+                    </div>
+
+                      <div className='col-md-4 '>
+                    <div className='card    text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>School Management System</h5>
+                          <small className='card-text'>School website and portal, the admin has can apply online </small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Vuejs, Bootstrap</small><br/>
+                          <Link to="https://github.com/Agboola12/School-Management"  className='text-light'><i class="fa fa-globe" aria-hidden="true"></i>visit</Link>
+
+                        </div>
+                      </div>
+                    </div>
+
+                      <div className='col-md-4 '>
+                    <div className='card   text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>Church Website</h5>
+                          <small className='card-text'>Rccg Church website, for people to know more about the Church</small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Angular and Bootstrap</small><br/>
+                          <Link to="https://churchwork.netlify.app"  className='text-light'><i class="fa fa-globe" aria-hidden="true"></i>visit</Link>
+                        </div>
+                      </div>
+                    </div>  
+                </div>
+
+                <div className='row mt-4  offset-2'>
+                    <div className='col-md-4 '>
+                    <div className='card  text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>Food App</h5>
+                          <small className='card-text'>The Food app helps people to order food online, it helps to save time and energy.</small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Reactjs and Nodejs </small>
+                          <small>The project is on the building process</small>
+                          
+                        </div>
+                      </div>
+                    </div>
+
+                      <div className='col-md-4 '>
+                    <div className='card   text-light'>
+                        <div className='card-body'>
+                          <h5 className='card-title '>Net Artist</h5>
+                          <small className='card-text'>Net Artist is a site, people can check for any artist or band dance to perform on their occassions.</small><br/><br/>
+                          <small><b>Technologies:</b></small><br/>
+                          <small>Vuejs and PhpMysqli</small> <br/>
+                          <small>The project is on the building process</small>
+                        </div>
+                      </div>
+                    </div>  
+                </div>
+              </div>
+          </div>
+
+          <div className='container-fluid text-center  mt-5' id='client'>
+            <div className='row p-5'>
+              <div className='col-md-4 offset-2'>
+                <p> 6     <br/> Projects Completed </p>
+                </div>
+                <div className='col-md-4'>
+               <p>1 <br/> Happy Client</p> 
+              </div>
+            </div>   
+          </div>
+
+          <div className='container text-center  p-3 text-light' >
+            <div className='row p-5'>
+              <div className='col-md-4 offset-2'>
+  <Link to="https://www.linkedin.com/in/elijah-iyanuoluwa-agboola-6b2631203" className="nav-link text-light  fs-7"><i className="fa fa-linkedin text-success mr-1" aria-hidden="true"></i>LinkedIn</Link>
+
+                </div>
+                <div className='col-md-4'>
+              <Link to="https://github.com/Agboola12" className="nav-link text-light fs-7"><i className="fa fa-github text-success mr-1" aria-hidden="true"></i>Github</Link>
+                
+              </div>
+            </div> 
+              <small className='text-center'>© 2023 BlackMan, All Rights Reserved</small>  
+
+          </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
