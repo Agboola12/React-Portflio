103fd617d9492a3a51ae526ae9fb4e5444ae2baa
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-14 15:00:12 +0100

    adding some changes

--- src/App.js
+++ src/App.js
@@ -7,22 +7,26 @@ import agbo from './agbo.png'
 function App() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg  ">
+      {/* <nav className="navbar navbar-expand-lg  ">
         
           <ul className="navbar-nav mx-auto" >
             <li className="nav-item">
               
-              <Link to="https://www.linkedin.com/in/elijah-iyanuoluwa-agboola-6b2631203" className="nav-link text-light  fs-7"><i className="fa fa-linkedin text-success mr-1" aria-hidden="true"></i>LinkedIn</Link>
             </li>
             <li className="nav-item">
-              <Link to="https://github.com/Agboola12" className="nav-link text-light fs-7"><i className="fa fa-github text-success mr-1" aria-hidden="true"></i>Github</Link>
             </li>
             <li className="nav-item">
-              <Link to="https://wa.link/f4r799" className="nav-link text-light  fs-7"><i className="fa fa-whatsapp text-success mr-1" aria-hidden="true"></i>Whatsapp</Link>
             </li>
-          </ul>
-      </nav>
-      
+            </ul>
+          </nav> */}
+
+          <div className='d-flex text-center' id='nav'>
+          <Link to="https://www.linkedin.com/in/elijah-iyanuoluwa-agboola-6b2631203" className="nav-link  text-light  fs-7"><i className="fa fa-linkedin text-success mr-1" aria-hidden="true"></i>LinkedIn</Link>
+          <Link to="https://github.com/Agboola12" className="nav-link  text-light fs-7"><i className="fa fa-github text-success mr-1" aria-hidden="true"></i>Github</Link>
+          <Link to="https://wa.link/f4r799" className="nav-link text-light  fs-7"><i className="fa fa-whatsapp text-success mr-1" aria-hidden="true"></i>Whatsapp</Link>
+
+          </div>
+
       <header className=''>
           <div className='container text-center text-light pt-5'>
                 <img src={agbo}  alt="agboimage" />
